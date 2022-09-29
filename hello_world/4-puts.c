6e276ce6f3991e364f97545e0f6e21349fcf468e
rrarima <5708@holbertonstudents.com>
2022-09-29 15:01:53 -0700

    display text on screen

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-int main(void){
-  /**
-   *main - Entry point
-   *
-   *Return: Always 0 (Success)
-   */
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return (0);
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
