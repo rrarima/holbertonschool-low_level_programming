726bf53ca1fc4d56717067f71152e91972c06bb6
rrarima <5708@holbertonstudents.com>
2022-10-03 02:29:54 -0700

    print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "_putchar"
+/**
+* main - Entry point
+*
+* Description: 'print single digit combinations'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
