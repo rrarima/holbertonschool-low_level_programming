3885c87a423267460704b378f72cabd4cc62d5c4
rrarima <5708@holbertonstudents.com>
2022-10-02 21:52:18 -0700

    reverse lower case alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'revers lower case alphabet'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char z = 'z';
+	
+	while (z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+	return (0);
+}
