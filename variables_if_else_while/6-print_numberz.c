15643b8f41f2f6aaaa7e409a4d3f5c8d24724045
rrarima <5708@holbertonstudents.com>
2022-10-02 21:32:47 -0700

    print single digit numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'print alphabet lowercase'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
