998f4e9dce31118f0b93311f99c21ccbecee8d87
rrarima <5708@holbertonstudents.com>
2022-10-04 01:04:00 -0700

    print lowercase alphabet

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,12 +2,17 @@
 /**
  * _islower - check for lowercase character
  * 
- * Return: 0
+ * @c: char input from user
+ * Return: 1
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return (1);
+	{
+		 return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
