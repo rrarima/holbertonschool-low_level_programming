22f07da2f60077e9adcca855d8f1950266d31757
rrarima <5708@holbertonstudents.com>
2022-10-04 14:26:19 -0700

    compute absolute int

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Prints absolute value of int.
+ * @n: number to be printed
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
