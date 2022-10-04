fc382c68e59a7bc5304ac4f5dc35f278e685a2a3
rrarima <5708@holbertonstudents.com>
2022-10-04 00:24:35 -0700

    print alphabet 10x

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - check the code
+ * print_alphabet_x1 - print alohabet 10x
  *
- * Description: 'display lowercase alphabet'
- * Return: Always 0.
+ *
+ *
  */
 void print_alphabet_x10(void)
 {
