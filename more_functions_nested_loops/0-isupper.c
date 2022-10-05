54136e4eb59fdcf1b33ca4d325554b4459f8f553
rrarima <5708@holbertonstudents.com>
2022-10-05 15:25:32 -0700

    add readme and return check if letter is uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_isalpha - controls if a character is alphabetical
+*
+*@c: character to be verified
+*
+*Return: return 0 or 1
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
