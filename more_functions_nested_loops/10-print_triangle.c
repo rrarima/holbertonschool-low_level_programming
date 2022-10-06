b0b010babe9c0fbf647740bca19f3953a33d4a8c
rrarima <5708@holbertonstudents.com>
2022-10-06 03:51:43 -0700

    make triangle :O

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ *@size: using int
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i = 1;
+	int a;
+
+	while (i <= size && size > 0)
+	{
+		a = 0;
+		while (a < size - i)
+		{
+			_putchar(' ');
+			a++;
+		}
+		a = 0;
+		while (a < i)
+		{
+			_putchar('#');
+			a++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+	{
+		_putchar('\n');
+	}
+}
