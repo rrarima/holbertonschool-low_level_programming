fe7100e1a50f759a440b7648db2ec6efe6e1cbd7
rrarima <5708@holbertonstudents.com>
2022-10-06 00:08:22 -0700

    print 0-9 expect 2&4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0-9 except 2 & 4.
+ *
+ *
+ * Return: 0.
+ */
+void print_most_numbers(void)
+{
+	char n = 48;
+
+	while (n < 58)
+	{
+		if (n == 50 || n == 52)
+		{
+			n++;
+			continue;
+		}
+		else
+		{
+			_putchar(n++)
+		}
+	}
+	_putchar('\n');
+}
