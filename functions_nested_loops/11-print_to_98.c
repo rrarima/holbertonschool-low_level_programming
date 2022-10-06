9d845498d1d82a84931ae5af9412b531d4cc398a
rrarima <5708@holbertonstudents.com>
2022-10-06 05:17:01 -0700

    print up to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Print table up to 98.
  * Return: table
  */
 void print_to_98(int n)
 {
-	while (n > 0)
+	if (n >= 98)
 	{
-		if (n < 10 && n > 0)
+		while (n > 98)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-			n++;
-		}
-		else if (n > 10 && n < 98)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			n++;
-		}
-		else if (n > 98)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			n--;
+			printf("%d, ", n--);
 		}
 	}
-	_putchar(57);
-	_putchar(56);
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d ", n++);
+		}
+		printf("%d\n", n);
+	}
 }
