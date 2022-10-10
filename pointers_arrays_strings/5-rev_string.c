d931ea5fdec724b91c9253202e6491bab32ecebf
rrarima <5708@holbertonstudents.com>
2022-10-10 02:50:20 -0700

    reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* rev_string - reverse string
+* @s: reverse a string
+* Return: reverse of string
+**/
+void rev_string(char *s)
+{
+	if(s != NULL)
+	{
+		size_t len = strlen(s);
+		char *end = s + (len > 1 ? len - 1 : 0);
+		char *aux = s;
+
+		for (len /=2; len; --len)
+		{
+			char tmp = *aux;
+			*aux++ = *end;
+			*end -- = tmp;
+		}
+	}
+}
