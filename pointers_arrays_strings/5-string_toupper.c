b8387f961451808ae44f11ac7516128c47012ca4
rrarima <5708@holbertonstudents.com>
2022-10-11 20:38:20 -0700

    print strings to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * string_toupper - chane lowercase letters of string to uppercase
+ * @str: take string input
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	/* iterate through array values*/
+	while (str[i] != '\0')
+	{
+		/*search for all lowercase letters*/
+		if (str[i] > 96 && str[i] < 123)
+		{
+			/**
+			 * Subtracts 32 from ascii code lower case
+			 * and always get upper case equivalent
+			 */
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return(str);
+}
