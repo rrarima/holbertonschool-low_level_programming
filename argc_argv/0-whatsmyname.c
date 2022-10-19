0084731e2013b3eb0ba1e0c2808473a7d93fa8fe
rrarima <5708@holbertonstudents.com>
2022-10-18 22:45:25 -0700

    program that prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main (int argc, char* argv[])
+/**
+ * main - print name of file
+ * Return: 0
+ * @argc: arg count
+ * @argv: arg val
+ */
+
+int main(int argc, char *argv[])
 {
 	int i;
-	for (i=0; i < argc; ++i)
+
+	for (i = 0; i < argc; i = i + 1)
 	{
-	printf("%s\n", argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
