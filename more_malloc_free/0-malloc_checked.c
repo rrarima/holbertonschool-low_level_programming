3d8db87fb4094435d926a62a32539f1ad1402490
rrarima <5708@holbertonstudents.com>
2022-10-25 16:59:50 -0700

    Add README and first task

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocate memory with malloc
+ * @b: unsigned int type
+ * Return: return pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *cmpb = malloc(b);
+
+	if (cmpb == NULL)
+	{
+		exit(98);
+	}
+	return(cmpb);
+}
