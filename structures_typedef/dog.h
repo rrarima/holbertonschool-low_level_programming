ab67013bd1f4c58d65c5b6993c137eee725c067c
rrarima <5708@holbertonstudents.com>
2022-10-30 13:18:41 -0700

    README and first task

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct my_struct - Short description
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner name
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
