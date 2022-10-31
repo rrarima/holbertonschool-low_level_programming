0c883e55e80927f94ae9038fa8c9e506aca10374
rrarima <5708@holbertonstudents.com>
2022-10-30 20:11:51 -0700

    print struct_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,8 +11,21 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-
-	printf("Name: %s\n", d->name);
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
