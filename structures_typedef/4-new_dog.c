cbacf10b1be02a5408fbc8b8ddcef167aaaf26d1
rrarima <5708@holbertonstudents.com>
2022-10-31 00:38:11 -0700

    write a function that creates a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include <stdlib.h>
+#include "functions.h"
+/**
+ * *new_dog - function to create new dog struct
+ * @name: store dog name
+ * @age: age
+ * @owner: store owner
+ * Return: newd
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	unsigned int name_size = sizeof(char) * (_strlen(name) + 1);
+	unsigned int owner_size = sizeof(char) * (_strlen(owner) + 1);
+
+	dog = malloc(sizeof(struct dog));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+
+	if (name == NULL)
+	{
+		return (NULL);
+	}
+	dog->name = malloc(name_size);
+	if (dog->name == NULL)
+	{
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+
+	dog->age = age;
+
+	if (owner == NULL)
+	{
+		return (NULL);
+	}
+	dog->owner = malloc(owner_size);
+	if (dog->owner == NULL)
+	{
+		return (NULL);
+	}
+	_strcpy(dog->owner, owner);
+
+	return (dog);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,9 +13,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+}dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
