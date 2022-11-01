9bf7d4d656b46c38aa980f9f23ced262caa1546c
rrarima <5708@holbertonstudents.com>
2022-11-01 14:39:01 -0700

    Add README and first task

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ *print_name - print name
+ *@name: name to print
+ *@f: function to print name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+
+	f(name);
+}
