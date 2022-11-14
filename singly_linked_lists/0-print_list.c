e03980ff9dbd38c13da41a14f9d39d0214bee120
rrarima <5708@holbertonstudents.com>
2022-11-13 23:12:56 -0800

    singly linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *ptr = NULL;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s \n", ptr->len, ptr->str);
+		}
+		ptr = ptr->next;
+		count = count + 1;
+	}
+	return (count);
+}
