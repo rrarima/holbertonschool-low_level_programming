2b6198885afcdf017fa33e7fe76ead405bd0a44f
rrarima <5708@holbertonstudents.com>
2022-11-13 23:18:48 -0800

    code fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (ptr->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)");
 		}
 		else
 		{
-			printf("[%d] %s \n", ptr->len, ptr->str);
+			printf("[%d] %s \n", h->len, h->str);
 		}
 		h = h->next;
 		count = count + 1;
