3f3cf08210e612c3a0057e103cd89f166753590d
rrarima <5708@holbertonstudents.com>
2022-11-18 01:51:38 -0800

    first task

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+  * print_list - Prints all elements of a list
+  * @h: A linked list
+  *
+  * Return: count
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		count = count + 1;
+	}
+	return (count);
+}
