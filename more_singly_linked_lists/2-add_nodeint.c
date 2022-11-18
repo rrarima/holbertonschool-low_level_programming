df83aa65394d2c290a02601f0b213ea69cfa2f8b
rrarima <5708@holbertonstudents.com>
2022-11-18 02:24:22 -0800

    betty fix

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
   * add_nodeint - add node to link list
-  * @h: A linked list
-  *
+  * @head: A linked list
+  * @n: int
   * Return: temp
   */
 listint_t *add_nodeint(listint_t **head, const int n)
