c6a0081f11dac0f6f62f39ac66a4634c39d0fa0a
rrarima <5708@holbertonstudents.com>
2022-11-20 17:44:59 -0800

    code fix and conversion using << math

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[i] != '\0')
 	{
-		if (b[i] != '0' && b[i] !='1' || b == NULL)
+		if ((b[i] != '0' && b[i] !='1') || b == NULL)
 		{
 			return (0);
 		}
