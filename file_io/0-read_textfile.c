cedb522ed8364133585047216cedb723b178945d
rrarima <5708@holbertonstudents.com>
2022-11-22 01:22:33 -0800

    clean

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,39 +7,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	//char *buff;
+	char *buff;
 	int fd, rd, wr;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	/*buff = malloc(sizeof(*buff) * letters);
+	buff = malloc(sizeof(*buff) * letters);
 	if (buff == NULL)
 	{
 		return (0);
-		}*/
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		//free(buff);
+		free(buff);
 		return (0);
 	}
-	/*rd = read(fd, buff, letters);
+	rd = read(fd, buff, letters);
 	if (rd == -1)
 	{
-		//free(buff);
+		free(buff);
 		close(fd);
 		return (0);
-		}*/
+	}
 	wr = write(STDOUT_FILENO, buff, rd);
 	if (wr == -1)
 	{
-		//free(buff);
+		free(buff);
 		close(fd);
 		return (0);
 	}
-	//free(buff);
+	free(buff);
 	close(fd);
 	return (wr);
 }
