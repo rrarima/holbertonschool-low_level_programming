d316c0d94540c8de3ab1b7d74fcab745f7496bc6
rrarima <5708@holbertonstudents.com>
2022-11-21 17:36:38 -0800

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - function that creates a file
  * @filename: name of file
  * @text_content: text to write to file
- * Return:
+ * Return: 1 on success o -1 if fail
  */
 int create_file(const char *filename, char *text_content)
 {
