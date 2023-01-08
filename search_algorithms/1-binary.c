bd1bcfd9f54f281559e0450b092b1f1105124b2f
rrarima <5708@holbertonstudents.com>
2023-01-07 18:04:18 -0800

    main_3.c caseattempt

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -13,6 +13,11 @@ int binary_search(int *array, size_t size, int value)
 	size_t left = 0;
 	size_t right = size - 1;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	while (left <= right)
 	{
 		size_t middle = left + (right - left) / 2;
